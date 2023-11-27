c1231d3fe8b956ef9e9093049b528c3bdd1ccb03
FaresAriandha <AkunKoding2009@gmail.com>
2023-11-27 22:09:31 +0700

    Initial Project

---
+++ app/src/main/java/com/alfares/clothizeapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.alfares.clothizeapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
