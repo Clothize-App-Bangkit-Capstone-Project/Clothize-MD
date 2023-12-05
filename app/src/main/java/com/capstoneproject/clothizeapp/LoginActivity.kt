ba408d64507a63cea863778282918bf08d59a2f4
sahlanputrap <sahlanpratama6@gmail.com>
2023-12-05 21:34:09 +0700

    menambahkan layout login dan regis

---
+++ app/src/main/java/com/capstoneproject/clothizeapp/LoginActivity.kt
@@ -0,0 +1,20 @@
+package com.capstoneproject.clothizeapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstoneproject.clothizeapp.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding =ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvIntentRegis.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+    }
+}
