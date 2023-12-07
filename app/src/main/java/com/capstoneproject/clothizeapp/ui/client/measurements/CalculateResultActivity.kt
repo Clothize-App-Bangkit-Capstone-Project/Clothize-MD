ba55a60c30c3eefc04efea5b5573aa67a9d4b897
FaresAriandha <AkunKoding2009@gmail.com>
2023-12-07 21:26:51 +0700

    Add Measurement and Result Page

---
+++ app/src/main/java/com/capstoneproject/clothizeapp/ui/client/measurements/CalculateResultActivity.kt
@@ -0,0 +1,14 @@
+package com.capstoneproject.clothizeapp.ui.client.measurements
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.capstoneproject.clothizeapp.databinding.ActivityCalculateResultBinding
+
+class CalculateResultActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCalculateResultBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCalculateResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
