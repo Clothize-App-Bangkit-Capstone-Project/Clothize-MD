5daa86dff1c4a970ddfa86af026f77585628613a
FaresAriandha <AkunKoding2009@gmail.com>
2023-12-07 12:20:44 +0700

    Finishing Login and Register

---
+++ app/src/main/java/com/capstoneproject/clothizeapp/ui/client/measurements/MeasurementActivity.kt
@@ -0,0 +1,12 @@
+package com.capstoneproject.clothizeapp.ui.client.measurements
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.capstoneproject.clothizeapp.R
+
+class MeasurementActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_measurement)
+    }
+}
